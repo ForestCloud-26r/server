920459bf733658a993614b0c72af2552f2b6867b
Kiril Yakymchuk <stbestich@gmail.com>
2025-07-17 18:35:47 +0300

    fix: small swagger fix

--- libs/shared/src/dtos/file.dto.ts
+++ libs/shared/src/dtos/file.dto.ts
@@ -1,46 +1,46 @@
 import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
-export declare class FileDto {
+export class FileDto {
   @Expose()
   @ApiProperty({ example: '0197d0ae-ab4a-7bf3-a32e-4fe889c2e2db' })
-  fileId: string;
+  fileId!: string;
 
   @Expose()
   @ApiProperty({ example: '0197d0ae-ab4a-7bf3-a32e-4fe889c2e2db' })
-  userId: string;
+  userId!: string;
 
   @Expose()
   @ApiProperty({ example: 'my-text.txt' })
-  fileName: string;
+  fileName!: string;
 
   @Expose()
   @ApiProperty({ example: '0197d0ae-ab4a-7bf3-a32e-4fe889c2e2db_my-text.txt' })
-  originalName: string;
+  originalName!: string;
 
   @Expose()
   @ApiProperty({ example: 'application/pdf' })
-  mimeType: string;
+  mimeType!: string;
 
   @Expose()
   @ApiProperty({ example: 5000 })
-  size: number;
+  size!: number;
 
   @Expose()
   @ApiProperty({
     example: './uploads/0197d0ae-ab4a-7bf3-a32e-4fe889c2e2db_my-text.txt',
   })
-  storagePath: string;
+  storagePath!: string;
 
   @Expose()
   @ApiProperty({ example: '2025-07-03T14:28:11.981Z' })
-  createdAt: Date;
+  createdAt!: Date;
 
   @Expose()
   @ApiProperty({ example: '2025-07-03T14:28:11.981Z' })
-  updatedAt: Date;
+  updatedAt!: Date;
 
   @Expose()
   @ApiProperty({ example: '2025-07-03T14:28:11.981Z', nullable: true })
-  declare deletedAt: Date | null;
+  deletedAt!: Date | null;
 }
